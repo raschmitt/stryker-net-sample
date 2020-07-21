863d3bcfb6495fdaca3e5ed1240c75c69709673e
Rogerio Schmitt <rogerio.schmitt@hbsis.com.br>
2020-07-21 18:15:21 -0300

    Ignore rider files

---
+++ Stryker.Net.Sample/Stryker.Net.Sample/Account.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Account
+{
+    public class Account
+    {
+        private double _overdraftLimit = 500;
+        
+        public Guid Id { get; private set; }
+        public string Owner { get; private set; }
+        public double Balance { get; private set; }
+        public bool Overdraft { get; private set; }
+
+        public Account(string owner, bool overdraft)
+        {
+            Id = Guid.NewGuid();
+            Owner = owner;
+            Balance = 0;
+            Overdraft = overdraft;
+        }
+
+        public void AddMoney(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void WithdrawMoney(double amount)
+        {
+            if (CanWithdraw(amount))
+                Balance -= amount;
+        }
+
+        private bool CanWithdraw(double amount)
+        {
+            if (Overdraft)
+                return (Balance + _overdraftLimit) >= amount;
+            
+            return Balance >= amount;
+        }
+    }
+}
