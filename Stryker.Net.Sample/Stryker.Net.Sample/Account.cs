84cc98824cf85f85a1de11c81db7dc734a5b4aff
Rogerio Schmitt <rogerio.schmitt@hbsis.com.br>
2020-07-31 15:47:04 -0300

    Update business logic

--- Stryker.Net.Sample/Stryker.Net.Sample/Account.cs
+++ Stryker.Net.Sample/Stryker.Net.Sample/Account.cs
@@ -4,21 +4,29 @@ namespace Stryker.Net.Sample
 {
     public class Account
     {
-        private double _overdraftLimit = 500;
+        private readonly double _overdraftLimit = 500;
         
         public Guid Id { get; private set; }
         public string Owner { get; private set; }
         public double Balance { get; private set; }
         public bool Overdraft { get; private set; }
 
-        public Account(string owner, bool overdraft)
+        public Account(string owner)
         {
             Id = Guid.NewGuid();
             Owner = owner;
-            Balance = 0;
-            Overdraft = overdraft;
         }
 
+        public void EnableOverdraft()
+        {
+            Overdraft = true;
+        }
+        
+        public void DisableOverdraft()
+        {
+            Overdraft = false;
+        }
+        
         public void AddMoney(double amount)
         {
             Balance += amount;
