bd2426c5ea34a4a2e82f3e4f4f68142f9f736ac7
Rogerio Schmitt <rogerio.schmitt@me.com>
2020-09-02 03:56:12 -0300

    Update InvestmentAccount.cs

--- Stryker.Net.Sample/Stryker.Net.Sample/InvestmentAccount.cs
+++ Stryker.Net.Sample/Stryker.Net.Sample/InvestmentAccount.cs
@@ -1,6 +1,6 @@
 ﻿namespace Stryker.Net.Sample
 {
-    //ToDo: Cover this class
+    //TODO: Cover this class
     public class InvestmentAccount: Account
     {
         public bool Leverage { get; private set; }
